da59a04bc6a05fb1bbfcc0b33e965dd859235d9a
diatsuda <sfffe@rambler.ru>
2024-07-25 18:46:45 +0700

    added class Cli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,12 @@
+package hexlet.code;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to brain games!");
+
+        String name = Cli.inString("May I get to know your name?");
+
+        System.out.println("Nice to meet you, " + name + "!");
     };
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli
+{
+    public static String inString(String message)
+    {
+        System.out.println(message);
+        Scanner in = new Scanner(System.in);
+        String answer = in.nextLine();
+        return answer;
+    }
+}
